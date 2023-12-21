05315a70f253b4968544c905b55449497e1a3607
Mat. Devski Carlier <carliermateo@outlook.be>
2023-12-21 15:31:23 +0100

    Created user roles, and initialized a default user for each role.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -137,6 +137,9 @@ namespace Dishcover.Areas.Identity.Pages.Account
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
+
+                    var role = _userManager.AddToRoleAsync(user, "User");
+
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Dishcover.Areas.Identity.Data;
+
 namespace Dishcover
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDBContextConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContextConnection") ??
+                                   throw new InvalidOperationException("Connection string 'ApplicationDBContextConnection' not found.");
 
             builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(connectionString));
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            builder.Services
+                .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDBContext>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
 
             var app = builder.Build();
 
@@ -32,7 +45,7 @@ namespace Dishcover
             app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseAuthentication();;
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
@@ -45,8 +58,58 @@ namespace Dishcover
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                ApplicationDBContext context = new ApplicationDBContext(services.GetRequiredService<DbContextOptions<ApplicationDBContext>>());
+                ApplicationDBContext context =
+                    new ApplicationDBContext(services.GetRequiredService<DbContextOptions<ApplicationDBContext>>());
                 ApplicationDBContext.DataInitializer(context);
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roles = new[] { "Admin", "Manager", "User" };
+
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // Create a user for each role
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                string email = "admin@neatbox.com";
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user = new ApplicationUser() { FirstName = "Super", LastName = "User", UserName = email, Email = email };
+
+                    await userManager.CreateAsync(user, "!Admin123");
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+
+                email = "sethye@neatbox.com";
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user = new ApplicationUser() { FirstName = "Synthe", LastName = "Wineplace", UserName = email, Email = email };
+
+                    await userManager.CreateAsync(user, "!Usr100");
+
+                    await userManager.AddToRoleAsync(user, "Manager");
+                }
+
+                email = "cjanade@neatbox.com";
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user3 = new ApplicationUser() { FirstName = "Steep", LastName = "Mountains", UserName = email, Email = email };
+
+                    await userManager.CreateAsync(user3, "!Usr100");
+                   
+                    await userManager.AddToRoleAsync(user3, "User");
+                }
             }
 
             app.Run();
